d0fdb656f1ce61d6073c137ad666652d6ab58869
Drummond <dillon.drummond@mymail.champlain.edu>
2022-09-22 14:22:54 -0400

    Adding a couple line for dijkstra

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -1,1 +1,16 @@
 #include "Agent.h"
+
+#include <queue>
+
+Path Agent::FindCatShortestPath(Point2D catPos) 
+{
+	std::priority_queue<int, Point2D, std::greater<int>> frontier;
+	//maybe use list of pair<cost, point>?
+
+	frontier.push(catPos);
+
+	while (!frontier.empty()) 
+	{
+		//if x coord is 0 or max x value or if y coord is 0 or max y value, return that path
+	}
+}
--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -2,12 +2,18 @@
 #define AGENT_H
 #include "Point2D.h"
 
+#include <list>
+
 class World;
+
+typedef std::list<Point2D> Path;
 
 class Agent {
 public:
   explicit Agent()= default;;
   virtual Point2D Move(World*)=0;
+
+  Path FindCatShortestPath(Point2D catPos);
 };
 
 #endif  // AGENT_H
